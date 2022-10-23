780cbf68bcd325fa653e741be3d15bb0d6fcb414
Rashidi Tayeb Zeynab <Z_Rashidi@ISC.CO.IR>
2022-10-23 13:57:02 +0330

    first commit

---
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -0,0 +1,14 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CicdApplication.class, args);
+    }
+
+}
